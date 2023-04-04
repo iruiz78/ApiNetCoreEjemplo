6a885c274ca341b0eaebb963f56cdfd5d10e6c2a
Ignacio <40893631+iruiz78@users.noreply.github.com>
2023-04-04 15:28:45 -0300

    Delete Modelos directory

--- Modelos/Factura.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication9.Modelos
-{
-    public class Factura
-    {
-        public int id { get; set; }
-        public string name { get; set; }
-        public string description { get; set; }
-    }
-}
